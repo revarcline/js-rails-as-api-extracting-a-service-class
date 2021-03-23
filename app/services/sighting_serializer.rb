216b3ac63a79b6ee72976329436aedb752c4fac9
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-23 17:02:03 -0500

    Done.

---
+++ app/services/sighting_serializer.rb
@@ -0,0 +1,16 @@
+class SightingSerializer
+  def initialize(sighting_object)
+    @sighting = sighting_object
+  end
+
+  def to_serialized_json
+    options = {
+      include: {
+        bird: { only: %i[name species] },
+        location: { only: %i[latitude longitude] }
+      },
+      except: %i[updated_at]
+    }
+    @sighting.to_json(options)
+  end
+end
